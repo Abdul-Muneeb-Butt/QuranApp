03309bbf9030eb3dd0e9c39e793ced6c86cb13c3
Abdul-Muneeb-Butt <247227@students.au.edu.pk>
2026-05-03 15:39:43 +0500

    add the class of http

--- QuranApp/Services/QiblaService.cs
+++ QuranApp/Services/QiblaService.cs
@@ -3,5 +3,11 @@ namespace QuranApp.Services
 {
     public class QiblaService
     {
+        private readonly HttpClient _http;
+
+        public QiblaService(HttpClient http)
+        {
+            _http = http;
+        }
     }
 }
